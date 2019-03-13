1edd727660055c5cb039fcdea7b165406559ee64
stefan <stefan.prisca@gmail.com>
2019-03-13 14:22:08 +0100

    Connect client.

--- tictactoe/tttclient_test.go
+++ tictactoe/tttclient_test.go
@@ -17,7 +17,10 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/hyperledger/fabric-sdk-go/pkg/client/channel"
 	mspclient "github.com/hyperledger/fabric-sdk-go/pkg/client/msp"
+	"github.com/hyperledger/fabric-sdk-go/pkg/client/resmgmt"
+	"github.com/hyperledger/fabric-sdk-go/pkg/common/errors/retry"
 	contextAPI "github.com/hyperledger/fabric-sdk-go/pkg/common/providers/context"
 	"github.com/hyperledger/fabric-sdk-go/test/integration"
 	"github.com/hyperledger/fabric-sdk-go/test/metadata"
@@ -26,6 +29,7 @@ import (
 	"github.com/hyperledger/fabric-sdk-go/pkg/core/config"
 	"github.com/hyperledger/fabric-sdk-go/pkg/fabsdk"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 const (
@@ -38,7 +42,7 @@ const (
 	org1User         = "User1"
 	org2User         = "User1"
 	channelID        = "tttchannel"
-	ccPath           = "github.com/example_cc"
+	ccId             = "ttt"
 )
 
 var (
@@ -53,6 +57,18 @@ var (
 	orgTestPeer1 fab.Peer
 	exampleCC    = "example_cc_e2e" + metadata.TestRunID
 )
+
+type multiorgContext struct {
+	// client contexts
+	ordererClientContext   contextAPI.ClientProvider
+	org1AdminClientContext contextAPI.ClientProvider
+	org2AdminClientContext contextAPI.ClientProvider
+	org1ResMgmt            *resmgmt.Client
+	org2ResMgmt            *resmgmt.Client
+	ccName                 string
+	ccVersion              string
+	channelID              string
+}
 
 /*
 	Test that will
@@ -77,18 +93,61 @@ func TestE2E(t *testing.T) {
 	integration.CleanupUserData(t, sdk)
 	defer integration.CleanupUserData(t, sdk)
 
-	org1MspClient, err = mspclient.New(sdk.Context(), mspclient.WithOrg(org1))
+	clientChannelContext := sdk.ChannelContext(channelID, fabsdk.WithUser(org1User), fabsdk.WithOrg(org1))
+	// Channel client is used to query and execute transactions (Org1 is default org)
+	client, err := channel.New(clientChannelContext)
+	require.NoError(t, err, "could not get channel client")
+
+	response, err := client.Query(channel.Request{ChaincodeID: ccID, Fcn: "invoke", Args: integration.ExampleCCDefaultQueryArgs()},
+		channel.WithRetry(retry.DefaultChannelOpts),
+		channel.WithTargetEndpoints(targetEndpoints...),
+	)
 	if err != nil {
-		t.Fatal(err, "failed to create org1MspClient")
+		t.Fatalf("Failed to query funds: %s", err)
 	}
 
-	org2MspClient, err = mspclient.New(sdk.Context(), mspclient.WithOrg(org2))
-	if err != nil {
-		t.Fatal(err, "failed to create org2MspClient")
-	}
+	return
 
-	loadOrgPeers(t, sdk.Context(fabsdk.WithUser(org1AdminUser), fabsdk.WithOrg(org1)))
+	// org1MspClient, err = mspclient.New(sdk.Context(), mspclient.WithOrg(org1))
+	// if err != nil {
+	// 	t.Fatal(err, "failed to create org1MspClient")
+	// }
 
+	// org2MspClient, err = mspclient.New(sdk.Context(), mspclient.WithOrg(org2))
+	// if err != nil {
+	// 	t.Fatal(err, "failed to create org2MspClient")
+	// }
+
+	// loadOrgPeers(t, sdk.Context(fabsdk.WithUser(org1AdminUser), fabsdk.WithOrg(org1)))
+	// loadOrgPeers(t, sdk.Context(fabsdk.WithUser(org2AdminUser), fabsdk.WithOrg(org2)))
+
+	// mc := multiorgContext{
+	// 	ordererClientContext:   sdk.Context(fabsdk.WithUser(ordererAdminUser), fabsdk.WithOrg(ordererOrgName)),
+	// 	org1AdminClientContext: sdk.Context(fabsdk.WithUser(org1AdminUser), fabsdk.WithOrg(org1)),
+	// 	org2AdminClientContext: sdk.Context(fabsdk.WithUser(org2AdminUser), fabsdk.WithOrg(org2)),
+	// 	ccName:                 exampleCC, // basic multi orgs test uses exampleCC for testing
+	// 	ccVersion:              "0",
+	// 	channelID:              channelID,
+	// }
+
+	// org1Peers, err := integration.DiscoverLocalPeers(mc.org1AdminClientContext, 1)
+	// require.NoError(t, err)
+	// _, err = integration.DiscoverLocalPeers(mc.org2AdminClientContext, 1)
+	// require.NoError(t, err)
+
+	// org1RMgmt, err := resmgmt.New(mc.org1AdminClientContext)
+	// require.NoError(t, err, "failed to create org1 resource management client")
+
+	// mc.org1ResMgmt = org1RMgmt
+
+	// // Org2 resource management client
+	// org2RMgmt, err := resmgmt.New(mc.org2AdminClientContext)
+	// require.NoError(t, err, "failed to create org2 resource management client")
+
+	// mc.org2ResMgmt = org2RMgmt
+	// joined, err := integration.IsJoinedChannel(channelID, mc.org1ResMgmt, org1Peers[0])
+	// require.NoError(t, err)
+	// fmt.Println("If it joined I go smoke.", joined)
 }
 
 func loadOrgPeers(t *testing.T, ctxProvider contextAPI.ClientProvider) {
